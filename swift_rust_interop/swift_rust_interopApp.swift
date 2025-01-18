065cc3852fefdc040974d10a5eae1bd68b692af0
Brandon Walton <bfwalton@comcast.net>
2025-01-18 09:43:49 -0500

    Initial Commit

---
+++ swift_rust_interop/swift_rust_interopApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_rust_interopApp.swift
+//  swift_rust_interop
+//
+//  Created by Brandon Walton on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_rust_interopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
